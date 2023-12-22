177ada75f49ae1f567d502f397829995a0efa081
lukino2000 <lukino2000@gmail.com>
2023-12-22 22:54:18 +0100

    initial commit

---
+++ custom_components/custom_filters/__init__.py
@@ -0,0 +1,32 @@
+"""Support custom filters for jinja2 templating"""
+import logging
+
+from homeassistant.helpers import template
+
+_LOGGER = logging.getLogger(__name__)
+
+_TemplateEnvironment = template.TemplateEnvironment
+
+## -- MAP ATTRIBUTES
+def mapattributes(self, list_of_dicts, list_of_keys):
+    l = []
+    for di in list_of_dicts:
+      newdi = { }
+      for key in list_of_keys:
+        newdi[key] = di[key]
+      l.append(newdi)
+    return l
+
+def init(*args):
+    """Initialize filters"""
+    env = _TemplateEnvironment(*args)
+    env.filters["mapattributes"] = mapattributes
+    return env
+
+template.TemplateEnvironment = init
+template._NO_HASS_ENV.filters["mapattributes"] = mapattributes
+
+async def async_setup(hass, hass_config):
+    tpl = template.Template("", template._NO_HASS_ENV.hass)
+    tpl._env.globals = mapattributes
+    return True
